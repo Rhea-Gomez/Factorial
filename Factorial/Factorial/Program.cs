54d885b43053c56f37c318008dd95c9701faf049
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-11 10:23:50 +0530

    Assignment on Factorial

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Factorial
+{
+    internal class Program
+    {
+        public static int Factorial(int num)
+        {
+            int factorial = num;
+            if(num == 1)
+            {
+                return factorial;
+            }
+            return factorial * Factorial(num - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to calculate its factorial: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Factorial of " + number + " is " +Factorial(number));
+
+        }
+    }
+}
